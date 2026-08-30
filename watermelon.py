b139c1646dba6742962a901422798fe1196b5eee
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-08-30 15:10:41 +0500

    Update watermelon.py

--- watermelon.py
+++ watermelon.py
@@ -1,16 +1,9 @@
-# You have a water melon.
-# You have to print YES if:
-#        1. The weight of water melon is divisible by 2.
-#        2. The half of the weight of the water melon is also divisible by 2.
-# Other wise print NO
+# Give a number in input check if it is even and half of of your input is also even. 
+# If theese all conditions are true print YES else print NO
 
-water_melon_weight = int(input())  # Getting the weight of the water melon
+water_melon_weight = int(input()) 
  
-if water_melon_weight > 2 and  water_melon_weight % 2 == 0: # Checking if it is divisible by 2 and only is the number which's half is odd.
-    print("Yes")  # Printing YES for situation
+if water_melon_weight > 2 and  water_melon_weight % 2 == 0: 
+    print("Yes")
 else:
-    print("No") # Printing NO for this situation.
-
-
-# Find the full problem here
-# https://www.codeforces.com/problemset/problem/4/A
+    print("No") 
