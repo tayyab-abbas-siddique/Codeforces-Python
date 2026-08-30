5963f602479546e92bb8c06e2355e7e008ba0423
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-08-30 15:01:32 +0500

    Update Way-Too-Long-Words.py

--- Way-Too-Long-Words.py
+++ Way-Too-Long-Words.py
@@ -1,10 +1,6 @@
-# You will given a string in input.
-# If the length of the string is greater than 10, you have to print it's first letter then length of the input minus 2 then last letter of word (without spaces).
-# Else print the input exactly.
-
 word = input()
 
 if len(word) > 10:
-    print(word[0]+str(len(word)-2)+word[-1])
+    print(word[0] + str(len(word)-2) + word[-1])
 else:
     print(word)
