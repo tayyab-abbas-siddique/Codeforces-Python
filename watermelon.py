67071622d8089db08574397fd53925152cf29655
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-09-08 09:16:00 +0500

    Update watermelon.py
    View full problem here
    https://codeforces.com/problemset/problem/4/A

--- watermelon.py
+++ watermelon.py
@@ -1,9 +1,9 @@
-# Give a number in input check if it is even and half of of your input is also even. 
-# If theese all conditions are true print YES else print NO
-
 water_melon_weight = int(input()) 
  
 if water_melon_weight > 2 and  water_melon_weight % 2 == 0: 
     print("Yes")
 else:
     print("No") 
+
+
+# https://codeforces.com/problemset/problem/4/A
