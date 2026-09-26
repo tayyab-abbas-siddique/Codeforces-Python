5ab42d1ae84e02ad4f00154923892d910bd88629
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-09-26 14:20:27 +0500

    Update Beutiful matrix.py

--- Beutiful matrix.py
+++ Beutiful matrix.py
@@ -1,15 +1,16 @@
 row = 0
 col = 0
- 
-for i in range(5):
-    arr = list(map(int, input().split()))
-    for j in range(5):
-        if arr[j] == 1:
-            row = i
-            col = j
- 
-moves = abs(row - 2) + abs(col - 2)
- 
-print(moves)
+
+if __name__ == "__main__":
+    for i in range(5):
+        arr = list(map(int, input().split()))
+        for j in range(5):
+            if arr[j] == 1:
+                row = i
+                col = j
+
+    moves = abs(row - 2) + abs(col - 2)
+
+    print(moves)
 
 # https://www.codeforces.com/problemset/problem/263/A
