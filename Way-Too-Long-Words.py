48425aa6490b444b47adc035862e98b3d9e0eeab
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-09-26 14:09:40 +0500

    Update Way-Too-Long-Words.py

--- Way-Too-Long-Words.py
+++ Way-Too-Long-Words.py
@@ -1,9 +1,11 @@
-word = input()
+word = input("Enter your string: ")
 
-if len(word) > 10:
-    print(word[0] + str(len(word)-2) + word[-1])
-else:
-    print(word)
+if __name__ == "__main__":
+    if len(word) > 10:
+        print(word[0] + str(len(word - 2)) + word[-1])
+    else:
+        print(word)
+
 
 
 # https://codeforces.com/problemset/problem/71/A
