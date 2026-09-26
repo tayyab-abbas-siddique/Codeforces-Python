62a758a5c25c9255bfbab70c52af2ff83405db15
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-09-26 14:17:01 +0500

    Update Next Round.py

--- Next Round.py
+++ Next Round.py
@@ -1,14 +1,15 @@
 n, k = list(map(int, input().split()))
 a = list(map(int, input().split()))
- 
-num = a[k-1]
-count = 0
-a_use = []
- 
-for i in a:
-    if i >= num and i != 0:
-        count += 1
-   
-print(count)
+
+if __name__ == "__main__":
+    num = a[k - 1]
+    count = 0
+    a_use = []
+
+    for i in a:
+        if i >= num and i != 0:
+            count += 1
+
+    print(count)
 
 # https://codeforces.com/problemset/problem/158/A
