fdb8d40101b8686fd06e5955d1b73960bc2ec7a4
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-09-26 14:18:52 +0500

    Update Bit++.py

--- Bit++.py
+++ Bit++.py
@@ -1,14 +1,16 @@
 cases = int(input())
- 
+
 x = 0
- 
-for i in range(cases):
-    word = input()
-    if "X++" in word or "++X" in word:
-        x = x + 1
-    if "X--" in word or "--X" in word:
-        x -= 1    
-        
-print(x)
+
+if __name__ == "__main__":
+    for i in range(cases):
+        word = input()
+        if "X++" in word or "++X" in word:
+            x += 1
+        if "X--" in word or "--X" in word:
+            x -= 1
+
+    print(x)
+
 
 #  https://codeforces.com/problemset/problem/282/A
