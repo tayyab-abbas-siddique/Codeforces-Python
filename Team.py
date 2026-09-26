0cbed3f00be6113689677a5ddf34115333e98c83
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-09-26 14:13:08 +0500

    Update Team.py

--- Team.py
+++ Team.py
@@ -1,12 +1,14 @@
 t = int(input())
 count = 0
-for i in range(t):
-    n = list(map(int, input().split()))
-    if sum(n) >= 2:
-        count += 1
-    else:
-        count += 0
-        
-print(count)
+
+if __name__ == "__main__":
+    for i in range(t):
+        n = list(map(int, input().split()))
+        if sum(n) >= 2:
+            count += 1
+        else:
+            count += 0
+
+    print(count)
 
 # https://codeforces.com/problemset/problem/231/A
