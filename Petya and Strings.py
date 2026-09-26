7974d9da28cf52eca93560241fe3834581e0af0d
Tayyab Abbas <tayyababbassiddique12@gmail.com>
2026-09-26 14:15:07 +0500

    Update Petya and Strings.py

--- Petya and Strings.py
+++ Petya and Strings.py
@@ -1,12 +1,15 @@
 a = input()
 b = input()
- 
-a = a.lower()
-b = b.lower()
- 
-if a < b:
-    print("-1")
-if a > b:
-    print("1")
-if a == b:
-    print("0")
+
+if __name__ == "__main__":
+    a = a.lower()
+    b = b.lower()
+
+    if a < b:
+        print("-1")
+    elif a > b:
+        print("1")
+    else:
+        print("0")
+
+# https://www.codeforces/problemset/problem/112A
